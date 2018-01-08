77909a13dc24a4b82bd5d2df1546a35ced130690
dwexid <xolaxote@gmail.com>
2018-01-08 13:40:25 +0700

    update features

--- app/models/jadwal.rb
+++ app/models/jadwal.rb
@@ -1,4 +1,8 @@
 class Jadwal < ApplicationRecord
     belongs_to :lapang, :foreign_key => "id_lapang"
     belongs_to :user, :foreign_key => "id_user"
+
+    validates :beg_time, presence: true
+    validates :end_time, presence: true
+    
 end
